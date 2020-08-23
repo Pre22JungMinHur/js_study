25a33f00bc77c213f947ba688a8401bde127ab46
HurBa <37764197+Pre22JungMinHur@users.noreply.github.com>
2020-08-23 23:30:26 +0900

    solution_1 수정
    solution_1 수정

--- algorithm/week_1/level_1-1/solution_1.js
+++ algorithm/week_1/level_1-1/solution_1.js
@@ -1,8 +1,21 @@
-function shift(line) {
-  let splittedLine = line.split('')
-	let a = splittedLine[0]
-	let b = splittedLine[2]
-  let result = `${Number(a)>>Number(b)}\n${Number(a)<<Number(b)}`
-  return result
+export const isString = (value) => {
+  return typeof value === 'string' ? true : 'String 값을 입력해주세요!'
+}
+const shift = (line) => {
+  if (isString(line)) {
+    let splittedLine = line.split('')
+    let num = ''
+    let shiftNum = ''
+    for (let i = 0; i < splittedLine.length; i++) {
+      if (splittedLine[i] === ' ') {
+        num = splittedLine.slice(0, i).join('')
+        shiftNum = splittedLine.slice(i + 1, splittedLine.length).join('')
+      }
+    }
+    let result = `${Number(num) >> Number(shiftNum)}\n${Number(num) << Number(shiftNum)}`
+    return result
+  }
+  return isString(line)
+
 }
 export default shift
--- algorithm/week_1/level_1-1/solution_1.test.js
+++ algorithm/week_1/level_1-1/solution_1.test.js
@@ -1,5 +1,13 @@
-import shift from './solution_1'
-
+import shift, {isString} from './solution_1'
+const typeErrorMessage = 'String 값을 입력해주세요!'
+describe ('isRightType', ()=>{
+  it('type이 맞지 않으면 typeErrorMessage를 반환합니다',()=>{
+    expect(isString(0)).toEqual(typeErrorMessage)
+    expect(isString(false)).toEqual(typeErrorMessage)
+    expect(isString([1,2,3])).toEqual(typeErrorMessage)
+    expect(isString({a:1,b:2})).toEqual(typeErrorMessage)
+  })
+})
 test('shift', () => {
   expect(shift('5 3')).toBe(`${5>>3}\n${5<<3}`);
 });
